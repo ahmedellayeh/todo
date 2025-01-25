075733edcb11717c6601c2cc5afec73182b760c4
ahmedellayeh <factsrandom811@gmail.com>
2025-01-25 20:36:16 +0200

    configured tasks , completed the project

--- src/components/todo/task/task.js
+++ src/components/todo/task/task.js
@@ -2,10 +2,14 @@ import React from "react";
 import './style.css';
 
 const Task = (props) => {
+    const [checked,check] = React.useState(props.checked);
+    const doaction = () => {
+        check(!checked);
+    }
     return (
         <div className="task">
-            <span className="task-name">{props.name}{"iaudfudugf"}</span>
-            <input type="checkbox" checked={props.checked}/>
+            <span className="task-name">{props.name}</span>
+            <input className="task-check" type="checkbox" checked={checked} onClick={doaction}/>
         </div>
     )
 }
--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -6,7 +6,13 @@ const Todo = () => {
     return (
         <div className='todo-list'>
             <div className='tasks'>
-                <Task name="what to do" checked={false}/>
+                <Task name="Task 1" checked={false}/>
+                <Task name="Task 2" checked={false}/>
+                <Task name="Task 3" checked={false}/>
+                <Task name="Task 4" checked={false}/>
+                <Task name="Task 5" checked={false}/>
+                <Task name="Task 6" checked={false}/>
+                <Task name="Task 7" checked={false}/>
             </div>
         </div>
     );
